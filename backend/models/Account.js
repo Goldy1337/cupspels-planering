3c780e2afce5e9e23b6a270940b6402d28b0762d
ironboy <thomas@nodehill.com>
2020-04-22 20:58:07 +0200

    another one

--- backend/models/Account.js
+++ backend/models/Account.js
@@ -4,10 +4,10 @@ const Schema = mongoose.Schema;
 const Types = Schema.Types;
 
 let schema = new Schema({
-    accountType: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    salt: { type: String, required: true }
+  accountType: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  salt: { type: String, required: true }
 });
 
 let model = mongoose.model(modelName, schema);
