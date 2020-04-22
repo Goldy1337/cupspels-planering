56e138094d74cb7b25e1c75dd76cfa472649411c
ironboy <thomas@nodehill.com>
2020-04-22 18:12:59 +0200

    spell length correclty

--- backend/models/CupPreset.js
+++ backend/models/CupPreset.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Types = Schema.Types;
 
-let schema = new Schema ({
-    cupType: {type: String, required: true},
-    matchLenght: {type: Number, required: true},
-    playersOnField: {type: Number, required: true}
+let schema = new Schema({
+    cupType: { type: String, required: true },
+    matchLength: { type: Number, required: true },
+    playersOnField: { type: Number, required: true }
 });
 
 let model = mongoose.model(modelName, schema);
