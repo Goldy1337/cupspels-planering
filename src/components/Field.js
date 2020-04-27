0db548b31165ee905872ba2da410a24de060d18d
Work <vikk@live.se>
2020-04-27 11:17:41 +0200

    Added true/false options via radio buttons to the Field-component, minor cleanup

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Button, ButtonGroup, Form, FormGroup, Input } from 'reactstrap'
+import { Button, ButtonGroup, Form, FormGroup, Input} from 'reactstrap'
 import mongoosy from 'mongoosy/frontend';
 const {
   Field
@@ -8,57 +8,60 @@ const {
 export default function NewField() {
   
 
-  const [field, setField] = useState({name: '', size: '', surface: '', outdoors: true})
+  const [field, setField] = useState({ name: '', size: '', surface: '', outdoors: true })
 
-    const updateField = update => setField({ ...field, ...update })
+  const updateField = update => setField({ ...field, ...update })
 
-    async function addFieldToDatabase(e) {
+  async function addFieldToDatabase(e) {
 
-        e.preventDefault()
-        
-        // Create a new field and save to db
-        let aField = new Field(field);
-        await aField.save();
-        // after saving the field it has an id
-        console.log('aField', aField.js);
-
-        // Read that field again from the db
-        let foundField = await Field.findOne({ _id: aField._id });
-        console.log('foundField', foundField.js);
-
-        // Read all fields from the db
-        let allFields = await Field.find();
-        console.log('allFields', allFields.js);
+    e.preventDefault()
       
-        // This won't reset the choice for outdoors/indoors
-        // to minimize clicking if several fields of the same
-        // type is to be added
-        updateField({name: '', size: '', surface: ''});
-    }
+    await Field.remove({})
+        
+    // Create a new field and save to db
+    let aField = new Field(field);
+    await aField.save();
+    // after saving the field it has an id
+    console.log('aField', aField.js);
 
-    return (
-        <div>
-            <Form onSubmit={addFieldToDatabase}>
-                <FormGroup>
-                    <Input type="text" placeholder="Add field name" 
-                    value={field.name} onChange={e => updateField({name: e.target.value})} 
-                    required>
-                    </Input>
-                    <Input type="number" placeholder="Add field size" 
-                    value={field.size} onChange={e => updateField({size: e.target.value})} 
-                    required>
-                    </Input>
-                    <Input type="text" placeholder="Add field surface" 
-                    value={field.surface} onChange={e => updateField({surface: e.target.value})}>
-                    </Input>
-                    {/* <ButtonGroup>
-                        <Button color="primary" onClick={() => setRSelected(1)} active={rSelected === 1}>Outdoors</Button>
-                        <Button color="primary" onClick={() => setRSelected(2)} active={rSelected === 2}>Indoors</Button>
-                    </ButtonGroup> */}
-                    <Button>Add Field</Button>
-                </FormGroup>
-            </Form>
-        </div>
-    )
+    // Read that field again from the db
+    let foundField = await Field.findOne({ _id: aField._id });
+    console.log('foundField', foundField.js);
+
+    // Read all fields from the db
+    let allFields = await Field.find();
+    console.log('allFields', allFields.js);
+      
+    // This won't reset the choice for outdoors/indoors
+    // to minimize clicking if several fields of the same
+    // type is to be added
+    updateField({name: '', size: '', surface: ''});
+  }
+
+  return (
+    <div>
+      <Form onSubmit={addFieldToDatabase}>
+        <FormGroup>
+          <Input type="text" placeholder="Add field name" 
+          value={field.name} onChange={e => updateField({name: e.target.value})} 
+          required>
+          </Input>
+          <Input type="number" placeholder="Add field size" 
+          value={field.size} onChange={e => updateField({size: e.target.value})} 
+          required>
+          </Input>
+          <Input type="text" placeholder="Add field surface" 
+          value={field.surface} onChange={e => updateField({surface: e.target.value})}>
+          </Input>
+          <ButtonGroup>
+            <Button color="primary" onClick={() =>updateField({outdoors: true})} active={field.outdoors === true}>Outdoors</Button>
+            <Button color="primary" onClick={() =>updateField({outdoors: false})} active={field.outdoors === false}>Indoors</Button>
+          </ButtonGroup>
+          <br></br>
+          <Button>Add Field</Button>
+        </FormGroup>
+      </Form>
+    </div>
+  )
 
 }
