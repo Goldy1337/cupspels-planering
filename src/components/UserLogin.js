6328bacc76d4917b9056ae3bfc2978adda70c5e4
Work <vikk@live.se>
2020-05-14 16:56:32 +0200

    Added rudimentary logic to create Member-tier accounts

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -22,10 +22,12 @@ const LoginHeader = (props) => {
   // const [email, setEmail] = useState('')
   // const [password, setPassword] = useState('')
   const [loginCredentials, setLoginCredentials] = useState({ email: '', password: '' })
-  const updateLoginCredentials = update => setLoginCredentials({...loginCredentials, ...update})
-
+  const updateLoginCredentials = update => setLoginCredentials({ ...loginCredentials, ...update })
+  
+  const [createMemberAccountCredentials, setCreateMemberAccountCredentials] = useState({ name: '', role: 'Member', email: '', phoneNumber: '', password: '', salt: 'notReallyNeeded' })
+  const updateCreateMemberAccountCredentials = update => setCreateMemberAccountCredentials({ ...createMemberAccountCredentials, ...update })
+  
   const [loginCheck, setLoginCheck] = useState('');
-  const [redirect, setRedirect] = useState(false);
 
   const toggleCreateAccount = () => setModalCreateAccount(!modalCreateAccount);
   const toggleLogin = () => setModalLogin(!modalLogin);
@@ -53,16 +55,16 @@ const LoginHeader = (props) => {
 
   const login = async e => {
     e.preventDefault()
-    // let user = await Login.login({ email, password})
     let user = await Login.login({ email: loginCredentials.email, password: loginCredentials.password });
     if (user.js.error) {
       console.log('Error')
-      // setEmail('')
-      // setPassword('')
+      updateLoginCredentials({email: '', password: ''})
     }
     else {
       updateLoginStatus({ user });
       console.log(user)
+      toggleLogin()
+      updateLoginCredentials({email: '', password: ''})
     }
   }
 
@@ -70,73 +72,107 @@ const LoginHeader = (props) => {
     e.preventDefault()
     await Login.logout()
     updateLoginStatus({ user: false })
+    let user = await Login.check()
+    console.log(user)
   }
-
+  //TODO Add checks to prohibit multiple accounts for the same email, etc
+  const createMemberAccount = async e => {
+    e.preventDefault()
+    let newMember = new User({
+      name: createMemberAccountCredentials.name,
+      role: createMemberAccountCredentials.role,
+      email: createMemberAccountCredentials.email,
+      phoneNumber: createMemberAccountCredentials.phoneNumber,
+      password: createMemberAccountCredentials.password,
+      salt: createMemberAccountCredentials.salt
+    })
+    await newMember.save()
+    if (newMember.js.error) {
+      console.log(newMember.error)
+    }
+    // else {
+    //   console.log()
+    //   toggleCreateAccount()
+    // }
+  }
+  //TODO Cleanup code
   return (
     <div>
       <Navbar className="loginHeader" color="info" dark>
         <NavbarBrand href="/" className="mr-auto loginHeaderText">Cupplanner</NavbarBrand>
-          <Nav navbar>
-            <NavItem>
-              <NavLink onClick={toggleCreateAccount}>Create Account</NavLink>
-            </NavItem>
-            <Modal isOpen={modalCreateAccount} toggle={modalCreateAccount} className={className}>
-              <ModalHeader toggleCreateAccount={toggleCreateAccount} close={closeBtnCreateAccount}>Create Account</ModalHeader>
-              <ModalBody>
-                <Form>
-                  <FormGroup row>
-                    <Label for="exampleEmail" sm={2}>Email</Label>
-                    <Col sm={10}>
-                      <Input type="email" name="accountCreationEmail" id="accountCreationEmailId" placeholder="Example@Email.com" required/>
-                    </Col>
-                  </FormGroup>
-                  <FormGroup row>
-                    <Label for="examplePassword" sm={2}>Password</Label>
-                    <Col sm={10}>
-                      <Input type="password" name="accountCreationPassword" id="accountCreationPasswordId" placeholder="Ex@mpl3~Pa$sw&rd" required/>
-                    </Col>
-                  </FormGroup>
-                </Form>
-              </ModalBody>
-              <ModalFooter>
-                <Button color="primary" onClick={toggleCreateAccount}>Do Something</Button>{' '}
-                <Button color="secondary" onClick={toggleCreateAccount}>Cancel</Button>
-              </ModalFooter>
-          </Modal>
-          {loginStatus.user ? <NavItem onClick={logout}>Logout</NavItem> : <NavItem onClick={toggleLogin}>Login</NavItem>}
-          {/* <NavItem>
-            if (loginStatus.user) {
-              <NavLink>Logout</NavLink>
-            } else {
-              <NavLink onClick={toggleLogin}>Login</NavLink>
-            }
-            </NavItem> */}
-            <Modal isOpen={modalLogin} toggle={modalLogin} className={className}>
-              <ModalHeader toggleLogin={toggleLogin} close={closeBtnLogin}>Login</ModalHeader>
+        <Nav navbar>
+          {loginStatus.user ? <NavLink>My Account</NavLink> : <NavLink onClick={toggleCreateAccount}>Create Account</NavLink>}
+          <Modal isOpen={modalCreateAccount} toggle={modalCreateAccount} className={className}>
+            <ModalHeader toggleCreateAccount={toggleCreateAccount} close={closeBtnCreateAccount}>Create Account</ModalHeader>
             <ModalBody>
               <Form>
                 <FormGroup row>
+                  <Label for="exampleName" sm={2}>Name</Label>
+                    <Col sm={10}>
+                  <Input type="text" name="accountCreationName" id="accountCreationNameId" placeholder="Name Namingson"
+                  value={createMemberAccountCredentials.name} onChange={e => updateCreateMemberAccountCredentials({name: e.target.value})}  required />
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Label for="examplePhoneNumber" sm={2}>Phone Number</Label>
+                    <Col sm={10}>
+                    <Input type="numbers" name="accountCreationPhoneNumber" id="accountCreationPhoneNumberId" placeholder="0701010101"
+                    value={createMemberAccountCredentials.phoneNumber} onChange={e => updateCreateMemberAccountCredentials({phoneNumber: e.target.value})} required />
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
                   <Label for="exampleEmail" sm={2}>Email</Label>
-                  <Col sm={10}>
-                    <Input type="email" name="loginEmail" id="loginEmailId" placeholder="Example@Email.com"
-                    value={loginCredentials.email} onChange={e => updateLoginCredentials({ email: e.target.value})}  required />
+                    <Col sm={10}>
+                    <Input type="email" name="accountCreationEmail" id="accountCreationEmailId" placeholder="Example@Email.com"
+                    value={createMemberAccountCredentials.email} onChange={e => updateCreateMemberAccountCredentials({email: e.target.value})} required/>
                   </Col>
                 </FormGroup>
                 <FormGroup row>
                   <Label for="examplePassword" sm={2}>Password</Label>
                   <Col sm={10}>
-                    <Input type="text" name="loginPassword" id="loginPasswordId" placeholder="Ex@mpl3~Pa$sw&rd"
-                      value={loginCredentials.password} onChange={e => updateLoginCredentials({ password: e.target.value})} required />
+                    <Input type="password" name="accountCreationPassword" id="accountCreationPasswordId" placeholder="Ex@mpl3~Pa$sw&rd"
+                    value={createMemberAccountCredentials.password} onChange={e => updateCreateMemberAccountCredentials({password: e.target.value})} required/>
                   </Col>
                 </FormGroup>
               </Form>
-              </ModalBody>
-              <ModalFooter>
-                <Button color="primary" onClick={login}>Do Something</Button>{' '}
-                <Button color="secondary" onClick={toggleLogin}>Cancel</Button>
-              </ModalFooter>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={createMemberAccount}>Create Account</Button>{' '}
+              <Button color="secondary" onClick={toggleCreateAccount}>Cancel</Button>
+            </ModalFooter>
+        </Modal>
+          {loginStatus.user ? <NavItem onClick={logout}>Logout</NavItem> : <NavItem onClick={toggleLogin}>Login</NavItem>}
+          <Modal isOpen={modalLogin} toggle={modalLogin} className={className}>
+            <ModalHeader toggleLogin={toggleLogin} close={closeBtnLogin}>Login</ModalHeader>
+          <ModalBody>
+            <Form>
+              <FormGroup row>
+                <Label for="exampleEmail" sm={2}>Email</Label>
+                <Col sm={10}>
+                  <Input type="email" name="loginEmail" id="loginEmailId" placeholder="Example@Email.com"
+                  value={loginCredentials.email} onChange={e => updateLoginCredentials({ email: e.target.value})}  required />
+                  </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label for="examplePassword" sm={2}>Password</Label>
+                <Col sm={10}>
+                  <Input type="password" name="loginPassword" id="loginPasswordId" placeholder="Ex@mpl3~Pa$sw&rd"
+                    value={loginCredentials.password} onChange={e => updateLoginCredentials({ password: e.target.value})} required />
+                </Col>
+              </FormGroup>
+            </Form>
+            <p>
+              Superadmin: god@gmail.com, 666
+              Testadmin: admin@gmail.com, 100
+              Testref: ref@gmail.com, 200
+            </p>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={login}>Login</Button>{' '}
+              <Button color="secondary" onClick={toggleLogin}>Cancel</Button>
+            </ModalFooter>
             </Modal>
-          </Nav>
+        </Nav>
       </Navbar>
     </div>
   );
