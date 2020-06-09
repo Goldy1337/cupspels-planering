19b92122b1eb08a2c4060ca39e1e0dd45f7bf0a1
Work <vikk@live.se>
2020-06-09 10:47:28 +0200

    Added new CSS/ability to change theme to RefereeList

--- src/components/RefereeList.js
+++ src/components/RefereeList.js
@@ -2,7 +2,8 @@ import React, { useContext } from 'react'
 import { Card,
   Container, 
   Row, 
-  Col } from 'reactstrap';
+  Col,
+  Table } from 'reactstrap';
   import { RefereeContext } from '../contexts/RefereeContextProvider'
 
 export default function RefereeList() {
@@ -11,38 +12,62 @@ export default function RefereeList() {
 
   const list = () => {
 
-    return referees.map((referee, i) => {
+    // return referees.map((referee, i) => {
       return (
-        
-        <Card 
-          key={referee._id + i}
-          body 
-          inverse 
-          className="mb-2 ref-list-card"
-          style={{ 
-            backgroundColor: 'lightgrey', 
-            borderColor: 'grey'
-          }}>
-            <Container>
-              <Row>
-                <span role="img" aria-label="book" style={{fontSize: '2em'}}>📕</span>
-              <Col>
-                <h1>{referee.name}</h1>
-                <h3>{referee.role}</h3>
-                <div class="ref-btm-card">
-                  <h3>Mail: {referee.email}</h3>
-                  <h3>Phone: {referee.phoneNumber}</h3>
-                </div>
-                </Col>
-                <span role="img"
-                aria-label="bin"
-                  style={{cursor: 'pointer'}}
-                >🗑️</span>
-              </Row>
-            </Container>
-          </Card>
-      )
-    })
+        <Table hover striped className="table-info">  
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Name</th>
+        <th>Role</th>
+        <th>Email</th>
+        <th>Phone Number</th>
+      </tr>
+    </thead>
+      {referees.map((referee, i) => {
+        return (
+          <tbody key={referee._id}>
+            <tr>
+              <td>{i + 1}</td>
+              <td>{referee.name}</td>
+              <td>{referee.role}</td>
+              <td>{referee.email}</td>
+              <td>{referee.phoneNumber}</td>
+            </tr>
+          </tbody>
+        )
+        })}
+      </Table>
+  );
+      //   <Card 
+      //     key={referee._id + i}
+      //     body 
+      //     inverse 
+      //     className="mb-2 ref-list-card"
+      //     style={{ 
+      //       backgroundColor: 'lightgrey', 
+      //       borderColor: 'grey'
+      //     }}>
+      //       <Container>
+      //         <Row>
+      //           <span role="img" aria-label="book" style={{fontSize: '2em'}}>📕</span>
+      //         <Col>
+      //           <h1>{referee.name}</h1>
+      //           <h3>{referee.role}</h3>
+      //           <div class="ref-btm-card">
+      //             <h3>Mail: {referee.email}</h3>
+      //             <h3>Phone: {referee.phoneNumber}</h3>
+      //           </div>
+      //           </Col>
+      //           <span role="img"
+      //           aria-label="bin"
+      //             style={{cursor: 'pointer'}}
+      //           >🗑️</span>
+      //         </Row>
+      //       </Container>
+      //     </Card>
+      // )
+    // })
   }
 
   return (
